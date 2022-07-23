8997ed46e3a5026b4c05d9158792cc7d29155d9e
Nella1a <m.allen@gmx.at>
2022-07-23 13:57:47 +0200

    Add html

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <p>Hello</p>
-
+      <div>
+        image
+      </div>
+      <article>
+        <h1>Preview</h1>
+        <h2>Gabrielle Essence Eau De Parfum</h2>
+        <p>A floral, solar and voluptuous interpretation composed by Olivier Polge,
+        Perfumer-Creator for the House of CHANEL.
+        </p>
+        <div>
+        $149.99
+          <span>$169.99</span>
+        </div>
+          <button>Add to Cart</button>
+        </article>
     </div>
   );
 }
