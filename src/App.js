d259ecbc19a48ac5927c40d2dd24d4c9e350817e
Nella1a <m.allen@gmx.at>
2022-07-23 16:16:51 +0200

    Add tailwindcss styles for body, image, divs, font, button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
+import React from 'react';
 
 // import logo from './logo.svg';
 
 function App() {
   return (
-    <div>
-      <div>
-        image
+    <div className="w-192 border-2 rounded-lg flex">
+      <div  >
+        <img src="/image-product-desktop.jpg" alt="chanel, bottle of parfume" className='w-96 rounded-l-lg'/>
       </div>
-      <article>
-        <h1 className="text-3xl font-bold underline">Preview</h1>
-        <h2>Gabrielle Essence Eau De Parfum</h2>
+      <article className="bg-white w-96 flex flex-col gap-1 justify-around p-4">
+        <h1 className="tracking-widest uppercase">Perfume</h1>
+        <h2 className="text-[#1c232b]">Gabrielle Essence Eau De Parfum</h2>
         <p>A floral, solar and voluptuous interpretation composed by Olivier Polge,
         Perfumer-Creator for the House of CHANEL.
         </p>
         <div>
-        $149.99
-          <span>$169.99</span>
+        <span className="text-[#3c8067]">$149.99</span>
+          $169.99
         </div>
-          <button>Add to Cart</button>
+          <button className="bg-[#3c8067] text-white w-80 h-14 rounded-lg">
+            Add to Cart</button>
         </article>
     </div>
   );
